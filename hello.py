a4b0ff24be5642f4853d5860c6252bb5bb86e416
aaroon84 <aaroon84@hotmail.com>
2017-02-02 15:16:59 +0100

    dont import qi

--- hello.py
+++ hello.py
@@ -9,6 +9,7 @@ import os
 #from importlib.machinery import SourceFileLoader
 #naoqi = SourceFileLoader("ALProxy", "naoqipythonlib/naoqi.py").load_module()
 import imp
+#qi = imp.load_source("", "naoqipythonlib/qi/qi.py")
 naoqi = imp.load_source("ALProxy", "naoqipythonlib/naoqi.py")
 #from naoqipythonlib/naoqi.py import ALProxy
 
